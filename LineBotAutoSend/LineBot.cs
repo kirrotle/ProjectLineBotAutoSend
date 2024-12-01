844411d0e1c2e8397d3912bb39a222311aa9294a
RDer <kirrotle@gmail.com>
2024-12-01 19:11:48 +0800

    加入專案檔案。

---
+++ LineBotAutoSend/LineBot.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.Json;
+using isRock.LineBot;
+
+namespace LineBotAutoSend
+{
+    internal class LineBot
+    {
+        private IConfigurationRoot _config;
+        private string _channelAccessToken;
+        private string _lineUserId;
+        private Bot _bot;
+
+        public LineBot()
+        {
+            _config = new ConfigurationBuilder().
+                SetBasePath(Directory.GetCurrentDirectory()).
+                AddJsonFile("appsettings.json").
+                Build();
+
+            _channelAccessToken = _config.GetSection("LineChannelAccessToken")?.Value?.ToString() ?? "";
+            _lineUserId = _config.GetSection("LineUserId")?.Value?.ToString() ?? "";
+            _bot = new Bot(_channelAccessToken);
+        }
+
+        public bool SendMessage(string message)
+        {
+            string result = _bot.PushMessage(_lineUserId, message);
+            return true;
+        }
+
+        public bool TestSendMessage()
+        {
+            string currentTime = DateTime.Now.ToString("F");
+            string message = $"現在時間為{currentTime}\n測試LineBot發送訊息正常";
+            _bot.PushMessage(_lineUserId,message);
+            return true;
+        }
+    }
+}
---
+++ LineBotAutoSend/Program.cs
@@ -0,0 +1,14 @@
+﻿
+
+namespace LineBotAutoSend
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("發送資訊");
+            LineBot lineBot = new LineBot();
+            lineBot.TestSendMessage();
+        }
+    }
+}
