6a27bcb70afb9991420bd859dc9b1f0f567c7283
RDer <watamiouo@gmail.com>
2024-12-04 00:25:27 +0800

    [modify]將計畫要做的事寫在README.md

--- LineBotAutoSend/Program.cs
+++ LineBotAutoSend/Program.cs
@@ -6,9 +6,12 @@ namespace LineBotAutoSend
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("發送資訊");
-            LineBot lineBot = new LineBot();
-            lineBot.TestSendMessage();
+            Console.WriteLine("蒐集資訊");
+
+
+            //Console.WriteLine("發送資訊");
+            //LineBot lineBot = new LineBot();
+            //lineBot.TestSendMessage();
         }
     }
 }
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # LineBotAutoSend
+
+取得鐵道跟絕區零的前瞻資訊
+並傳送Line至手機提醒
